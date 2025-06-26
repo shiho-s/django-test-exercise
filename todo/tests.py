eca3796c2a2f90bc8c542134e7f6ca797c14da82
1F10230131 <s1f102301314@iniad.org>
2025-06-26 11:35:01 +0900

    テストのサンプルを追加

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from django.utils import timezone
 
 # Create your tests here.
+class SampleTestCase(TestCase):
+    def test_sample(self):
+        self.assertEqual(1 + 2, 3)
