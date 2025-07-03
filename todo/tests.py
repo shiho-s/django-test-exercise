56a82f7c913869f0d8a1dcd77d0f30be540e0f0e
1F10230131 <s1f102301314@iniad.org>
2025-07-03 11:58:06 +0900

    コードを整形

--- todo/tests.py
+++ todo/tests.py
@@ -32,9 +32,11 @@ class TaskModelTestCase(TestCase):
 
     def test_over_future(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
-        #current = timezone.make_aware(datetime(2024, 6, 30, 0, 0, 0))
+        current = timezone.make_aware(datetime(2024, 6, 30, 0, 0, 0))
         task = Task(title='task1', due_at=due)
         task.save()
+
+        self.assertFakse(task.is_overdue(current))
 
     def test_is_overdue_past(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
@@ -45,8 +47,8 @@ class TaskModelTestCase(TestCase):
         self.assertTrue(task.is_overdue(current))
 
     def test_is_overdue_none(self):
-        #due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
-        #current = timezone.make_aware(datetime(2024, 6, 30, 0, 0, 0))
+        due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
+        current = timezone.make_aware(datetime(2024, 6, 30, 0, 0, 0))
         task = Task(title='task3', due_at=None)
         task.save()
 
